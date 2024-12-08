fa89d301ea11c856edfc23e64f05ada8cf874aa1
Lucia Heredia <luciaheredia22@gmail.com>
2024-12-08 21:44:27 +0200

    rest-api done

--- jokeApi.py
+++ jokeApi.py
@@ -1,8 +1,14 @@
+import requests
 
+api_url = 'https://v2.jokeapi.dev/joke/Programming?type=single' # category: Programming
 
-
-
-
-
-
+def get_joke():
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        return e
+    
+    data = response.json()
+    return data['joke']
 
