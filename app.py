24fba428b9a831ccbf865aaee9124ac4155e05db
Lucia Heredia <luciaheredia22@gmail.com>
2024-12-08 21:36:35 +0200

    flask done

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
+import jokeApi
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', joke_text=jokeApi.get_joke())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
